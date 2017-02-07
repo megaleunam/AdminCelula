726a7e990c8fdb2bd3fcaca3fd19ca01c428d0a8
Maria Mata <Maria Mata@MariaMata-PC>
2017-02-06 22:18:41 -0430

    inicio del Proyecto clase persona

---
+++ ALFA/src/AdminPersona/Persona.java
@@ -0,0 +1,20 @@
+/*
+ * Clase Persona 
+ * registra datos de la persona,
+ * la persona son integrantes de la celula, asisten a una celula.
+ * almacenados en la base de datos
+ */
+package AdminPersona;
+
+/**
+ *
+ * @author Manuel Mariño
+ */
+public class Persona {
+    String nombre;
+    String apellido;
+    String direcccion;
+    String email;
+    String telefono;
+    
+}
